fae50d94b45c6e323dcab8da426df71692021875
Jun-Sik Choi <leichoi91@gmail.com>
2021-04-29 17:18:07 +0900

    ADD: Owners / Dogs Data Models and Migration File

--- owners/models.py
+++ owners/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Owners(models.Model):
+    name = models.CharField("owner name", max_length=45)
+    email = models.CharField("owner email", max_length=300)
+    age = models.IntegerField("owner age")
+
+    def __str__(self):
+        return f"{self.id}: name: {self.name} email: {self.email} age: {self.age}"
+
+
+class Dogs(models.Model):
+    owner_id = models.ForeignKey(
+        "Owners", verbose_name="owner id", on_delete=models.CASCADE
+    )
+    name = models.CharField("dog name", max_length=45)
+    age = models.IntegerField("dot age")
+
+    def __str__(self):
+        return f"{self.id}: name: {self.name} age: {self.age} ownedBy: {self.owner_id}"
